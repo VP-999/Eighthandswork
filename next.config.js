d08223565db76c34f17e096121f1bd5816e3a328
Tanmoy027 <tanmoykumar027@gmail.com>
2025-06-11 17:54:55 +0600

    try to fix varsel deploy

--- next.config.js
+++ next.config.js
@@ -11,20 +11,6 @@ const nextConfig = {
     domains: ["v0.blob.com", "images.pexels.com"],
     unoptimized: true,
   },
-  webpack: (config, { isServer }) => {
-    // Add a rule to handle the Supabase Realtime client
-    config.module.rules.push({
-      test: /\.js$/,
-      include: /node_modules\/@supabase/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-        },
-      },
-    });
-    return config;
-  },
 }
 
 module.exports = nextConfig
